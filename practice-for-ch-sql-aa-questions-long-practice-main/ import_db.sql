135463d22ec98d370f8d7bc691641f4e341e4578
Elliot Kuok <elliotckuok@gmail.com>
2023-06-21 09:51:53 -0700

    Finish import_db

---
+++ practice-for-ch-sql-aa-questions-long-practice-main/ import_db.sql
@@ -0,0 +1,91 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  question_id INTEGER,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+  
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  questions_id TEXT NOT NULL,
+  parent_id TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (questions_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Justin', 'Aitken'),
+  ('Elliot', 'Kuok');
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('AA', 'A lame test', 1),
+  ('ij', 'wjfoiwejwe', 2);
+
+
+INSERT INTO
+  question_follows (user_id, question_follows)
+VALUES
+  (1, 'Why are we doing this?'),
+  (2, 'What is the point?');
+  
+INSERT INTO
+  replies (question_id, parent_id, user_id, body)
+VALUES
+  (1, 1, 1, "reply to question"),
+  (2, 2, 2, 'another reply to question');
+
+INSERT INTO
+  question_like (user_id, question_id)
+VALUES
+  (1, 1),
+  (2, 2);
+
+
